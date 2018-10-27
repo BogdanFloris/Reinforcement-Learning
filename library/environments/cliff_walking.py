4a9c2ae966e1a322658989a62db2ec110e14c6db
Bogdan Floris <bogdan.floris@gmail.com>
2018-10-27 18:03:03 +0200

    cliff walking env implemented

--- library/environments/cliff_walking.py
+++ library/environments/cliff_walking.py
@@ -2,7 +2,6 @@ import numpy as np
 import sys
 from gym.envs.toy_text import discrete
 
-
 UP = 0
 RIGHT = 1
 DOWN = 2
@@ -31,7 +30,7 @@ class CliffWalkingEnv(discrete.DiscreteEnv):
         self.shape = (5, 10)
         self.start_state_index = np.ravel_multi_index((4, 0), self.shape)
 
-        nS = np.prod(self.shape)
+        nS: int = np.prod(self.shape)
         nA = 4
 
         # Cliff location
@@ -40,6 +39,13 @@ class CliffWalkingEnv(discrete.DiscreteEnv):
 
         # Calculate transition probabilities and rewards
         P = {}
+        for s in range(nS):
+            position = np.unravel_index(s, self.shape)
+            P[s] = {a: [] for a in range(nA)}
+            P[s][UP] = self._calculate_transition_prob(position, [-1, 0])
+            P[s][RIGHT] = self._calculate_transition_prob(position, [0, 1])
+            P[s][DOWN] = self._calculate_transition_prob(position, [1, 0])
+            P[s][LEFT] = self._calculate_transition_prob(position, [0, -1])
 
         # Calculate initial state distribution
         # We always start in state (4, 0)
@@ -55,7 +61,48 @@ class CliffWalkingEnv(discrete.DiscreteEnv):
         :param delta: Change in position from transition
         :return: (1.0, new_state, reward, done)
         """
-        pass
+        new_position = np.array(current) + np.array(delta)
+        new_position = self._limit_coordinates(new_position).astype(int)
+        new_state = np.ravel_multi_index(tuple(new_position), self.shape)
+        if self._cliff[tuple(new_position)]:
+            return [(1.0, self.start_state_index, -100, False)]
+
+        terminal_state = (self.shape[0] - 1, self.shape[1] - 1)
+        is_done = tuple(new_position) == terminal_state
+        if is_done:
+            return [(1.0, new_state, 10, True)]
+        else:
+            return [(1.0, new_state, -1, False)]
+
+    def _limit_coordinates(self, position):
+        """
+        Prevents the agent from falling of the grid.
+        :param position: Current position on the grid as (row, col)
+        :return: Adjusted position
+        """
+        position[0] = max(0, min(position[0], self.shape[0] - 1))
+        position[1] = max(0, min(position[1], self.shape[1] - 1))
+        return position
 
     def render(self, mode='human'):
-        pass
+        outfile = sys.stdout
+
+        for s in range(self.nS):
+            position = np.unravel_index(s, self.shape)
+            if self.s == s:
+                output = " x "
+            elif position == (4, 9):
+                output = " G "
+            elif self._cliff[position]:
+                output = " C "
+            else:
+                output = " o "
+
+            if position[1] == 0:
+                output = output.lstrip()
+            if position[1] == self.shape[1] - 1:
+                output = output.rstrip()
+                output += '\n'
+
+            outfile.write(output)
+        outfile.write('\n')
