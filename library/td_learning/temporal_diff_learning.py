f1c51fd56153ee83e661f912037f896260378107
Bogdan Floris <bogdan.floris@gmail.com>
2019-03-31 19:59:54 +0200

    updated q learning to work with function approximation

--- library/td_learning/temporal_diff_learning.py
+++ library/td_learning/temporal_diff_learning.py
@@ -6,37 +6,54 @@ import numpy as np
 from collections import defaultdict
 from library import plotting
 from library.utils import make_epsilon_greedy_policy
+from library.estimators.estimators import Estimator
 from tqdm import tqdm
 
 
-def q_learning(env, num_episodes: int, q=None, discount_factor=1.0,
-               alpha=0.5, epsilon=0.1, distribute_prob=True):
+def q_learning(env, num_episodes: int, q=None,
+               estimator: Estimator = None,
+               discount_factor=1.0, alpha=0.5,
+               epsilon=0.1, epsilon_decay=1.0,
+               distribute_prob=True):
     """
     Q-Learning (off-policy control) algorithm implementation as described on page 131
     of the book Reinforcement Learning: An Introduction by Sutton and Barto.
     :param env: The OpenAI Env used
     :param num_episodes: Number of episodes to run the algorithm for
     :param q: Q action state values to start from
+    :param estimator: The estimator used to predict the q function
+                      in case of Q learning with function approximation
     :param discount_factor: The gamma discount factor
     :param alpha: The learning rate
     :param epsilon: Chance to sample a random action
+    :param epsilon_decay: Used to update the epsilon value as we move in time
     :param distribute_prob: Whether or not to distribute the probability between best actions
                             or just choose the first best action an assign it all the probability mass.
     :return: a tuple (q, stats) with q the optimal value function,
              and stats, statistics to be used for plotting
     """
     # initialize the action value function
-    if q is None:
+    if q is None and estimator is None:
         q = defaultdict(lambda: np.zeros(env.action_space.n))
+    # determine if we are in the discrete state case and using Q function
+    # or in the continuous state case and we are using an estimator
+    discrete = q is not None and estimator is None
     # initialize the statistics
     stats = plotting.EpisodeStats(
         episode_lengths=np.zeros(num_episodes),
         episode_rewards=np.zeros(num_episodes))
-    # initialize the policy
-    policy = make_epsilon_greedy_policy(env.action_space.n, epsilon=epsilon,
-                                        q=q, distribute_prob=distribute_prob)
+    policy = None
+    # initialize the policy for the Q function case
+    if discrete:
+        policy = make_epsilon_greedy_policy(env.action_space.n, epsilon=epsilon,
+                                            q=q, distribute_prob=distribute_prob)
     # loop for each episode
     for episode in tqdm(range(num_episodes)):
+        # initialize policy for the estimator case
+        if not discrete:
+            policy = make_epsilon_greedy_policy(env.action_space.n,
+                                                epsilon=epsilon * epsilon_decay ** episode,
+                                                estimator=estimator)
         # initialize the state
         state = env.reset()
         # loop for each step in the episode
@@ -44,20 +61,27 @@ def q_learning(env, num_episodes: int, q=None, discount_factor=1.0,
             # choose action from state based on the policy
             action_prob = policy(state)
             action = np.random.choice(np.arange(len(action_prob)), p=action_prob)
+            # take a step in the environment
             next_state, reward, done, _ = env.step(action)
             # update statistics
             stats.episode_rewards[episode] += reward
             stats.episode_lengths[episode] = t
-            # q learning update
-            best_next_action = np.argmax(q[next_state])
-            q[state][action] += alpha * (reward + discount_factor *
-                                         q[next_state][best_next_action] - q[state][action])
+            if discrete:
+                # q learning update for Q function
+                best_next_action = np.argmax(q[next_state])
+                q[state][action] += alpha * (reward + discount_factor *
+                                             q[next_state][best_next_action] - q[state][action])
+            else:
+                # q learning update for the estimator
+                q_values = estimator.predict(next_state)
+                td_update = reward + discount_factor * np.max(q_values)
+                estimator.update(state, action, td_update)
             # check for finished episode
             if done:
                 break
             # otherwise update state
             state = next_state
-    return q, stats
+    return q, stats if discrete else estimator, stats
 
 
 def sarsa(env, num_episodes: int, q=None, discount_factor=1.0,
