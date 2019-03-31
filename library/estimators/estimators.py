eddb83ecb29482071af99012a3cf4063f0b3d034
Bogdan Floris <bogdan.floris@gmail.com>
2019-03-31 18:25:21 +0200

    finished SGD estimator + updated plot function for mountain car

--- library/estimators/estimators.py
+++ library/estimators/estimators.py
@@ -1,6 +1,11 @@
 """
 Module that contains estimators used to approximate the value functions
 """
+import numpy as np
+from sklearn.pipeline import FeatureUnion
+from sklearn.preprocessing import StandardScaler
+from sklearn.kernel_approximation import RBFSampler
+from sklearn.linear_model import SGDRegressor
 
 
 class Estimator:
@@ -8,10 +13,6 @@ class Estimator:
     General estimator interface. Subclass this and implement
     the methods and use it to approximate the value functions.
     """
-    def __init__(self):
-        # one model for each discrete action
-        self.models = None
-
     def predict(self, state, action=None):
         """
         Predicts the value of the given state.
@@ -34,3 +35,51 @@ class Estimator:
         :return:
         """
         raise NotImplementedError
+
+
+class QFunctionSGD(Estimator):
+    def __init__(self, env):
+        # get state samples from the environment
+        # to fit the scaler and featurizer
+        env_samples = np.array([env.observation_space.sample() for _ in range(10000)])
+        # initialize standard scaler (scales to 0 mean and 1 std)
+        self.scaler = StandardScaler()
+        # 4 Radial Basis Functions with 100 components each concatenated
+        self.featurizer = FeatureUnion([
+            ("rbf1", RBFSampler(gamma=5.0, n_components=100)),
+            ("rbf2", RBFSampler(gamma=2.0, n_components=100)),
+            ("rbf3", RBFSampler(gamma=1.0, n_components=100)),
+            ("rbf4", RBFSampler(gamma=0.5, n_components=100))
+        ])
+        # fit the scaler and the featurizer
+        self.featurizer.fit(self.scaler.fit_transform(env_samples))
+        # initialize models, one model for each action
+        self.models = []
+        for _ in range(env.action_space.n):
+            # initialize model
+            model = SGDRegressor(learning_rate='constant')
+            # fit a value (from reset)
+            model.partial_fit([self.get_representation(env.reset())], [0])
+            # add to models
+            self.models.append(model)
+
+    def predict(self, state, action=None):
+        representation = self.get_representation(state)
+        if action is None:
+            return np.array([m.predict([representation])[0] for m in self.models])
+        else:
+            return self.models[action].predict([representation])[0]
+
+    def update(self, state, action, target):
+        representation = self.get_representation(state)
+        self.models[action].partial_fit([representation], [target])
+
+    def get_representation(self, state):
+        """
+        Get the representation of the given state
+        using the scaler and the featurizer
+        :param state: the state for which we are getting the representation
+        :return: the representation of the state
+        """
+        scaled = self.scaler.transform([state])
+        return self.featurizer.transform(scaled)[0]
--- library/plotting.py
+++ library/plotting.py
@@ -16,13 +16,14 @@ EpisodeStats = namedtuple("Stats", ["episode_lengths", "episode_rewards"])
 def plot_cost_to_go_mountain_car(env, estimator, num_tiles=20):
     x = np.linspace(env.observation_space.low[0], env.observation_space.high[0], num=num_tiles)
     y = np.linspace(env.observation_space.low[1], env.observation_space.high[1], num=num_tiles)
-    X, Y = np.meshgrid(x, y)
-    Z = np.apply_along_axis(lambda _: -np.max(estimator.predict(_)), 2, np.dstack([X, Y]))
+    x_grid, y_grid = np.meshgrid(x, y)
+    z_grid = np.apply_along_axis(lambda _: -np.max(estimator.predict(_)), 2,
+                                 np.dstack([x_grid, y_grid]))
 
     fig = plt.figure(figsize=(10, 5))
     ax = fig.add_subplot(111, projection='3d')
-    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
-                           cmap=matplotlib.cm.coolwarm, vmin=-1.0, vmax=1.0)
+    surf = ax.plot_surface(x_grid, y_grid, z_grid, rstride=1, cstride=1,
+                           cmap='viridis', vmin=-1.0, vmax=1.0)
     ax.set_xlabel('Position')
     ax.set_ylabel('Velocity')
     ax.set_zlabel('Value')
